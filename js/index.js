f51154b032a3a480a1305f73d516f9942b2f412b
Ilya <tkach.io123@gmail.com>
2020-06-23 15:34:49 +0300

    Big refactoring and adding class, refactoring with position of the empty block

--- js/index.js
+++ js/index.js
@@ -1,152 +1,158 @@
-const CELL_SIZE = 200;
-const cells = [];
+class PuzzleGame {
+    constructor(cellSize, appWidth, appHeight) {
+        this.cellSize = cellSize;
+        this.appWidth = appWidth;
+        this.appHeight = appHeight;
 
-const arr = [
-    [1,2,3,4],
-    [5,6,7,8],
-    [9,10,11,12],
-    [13,14,15,0]
-];
+        this.arr = [
+            [1,2,3,4],
+            [5,6,7,8],
+            [9,10,11,12],
+            [13,14,15,0] // 0 - позиция пустого блока
+        ];
 
-const emptyCell = {
-    left: 0,
-    top: 0
-};
+        this.emptyCell = {
+            left: 3,
+            top: 3
+        };
 
-cells.push(emptyCell);
-
-const app = new PIXI.Application(
-    {
-        width: 800,
-        height: 800,
-        backgroundColor: 0xAAAAAA
+        this.cells = [];
+        this.cells.push(this.emptyCell);
     }
-);
 
-document.querySelector('.field').appendChild(app.view);
+    createApp() {
+        this.app = new PIXI.Application(
+            {
+                width: this.appWidth,
+                height: this.appHeight,
+                backgroundColor: 0xAAAAAA
+            }
+        );
 
-function positioning(cell, top, left, CELL_SIZE) {
-    cell.x = left * CELL_SIZE;
-    cell.y = top * CELL_SIZE;
-    cell.left = left;
-    cell.top = top;
-}
-
-function isWrongBlock(cell) {
-    const leftSubtraction = Math.abs(emptyCell.left - cell.left);
-    const topSubtractions = Math.abs(emptyCell.top - cell.top);
-
-    return leftSubtraction + topSubtractions > 1
-}
-
-function moving(cell) {
-    if (isWrongBlock(cell)) return;
-
-    Ease.ease.add(cell,{x: emptyCell.left * CELL_SIZE, y: emptyCell.top * CELL_SIZE}, { duration: 1000 } );
-
-    const tempLeft = emptyCell.left;
-    const tempTop = emptyCell.top;
-
-    emptyCell.left = cell.left;
-    emptyCell.top = cell.top;
-    cell.left = tempLeft;
-    cell.top = tempTop;
-
-    win();
-}
-
-function createCells(i, left, top) {
-    if (i === 0) return;
-
-    let cell = new PIXI.Sprite.from(`images/${i}.png`);
-    positioning(cell, top, left, CELL_SIZE);
-    app.stage.addChild(cell);
-
-    cell.interactive = true;
-    cell.buttonMode = true;
-    cell.index = i;
-
-    cell.on("pointerup", () => {
-        moving(cell);
-    });
-
-    cells.push(cell);
-}
-
-function win() {
-    const isFinished = cells.every(cell => {
-            if (!cell.index) return true;
-        return cell.index === cell.top*4 + cell.left + 1; // cause we have a number
-    });
-    console.log(isFinished);
-
-    if (isFinished) alert("You win!")
-}
-
-function shuffle(x, y) { // 2, 3
-    let nullX = this.getNullCell().x; // 3
-    let nullY = this.getNullCell().y; // 3
-    console.log(nullX);
-    console.log(nullY);
-    if (
-        ((x - 1 === nullX || x + 1 === nullX) && y === nullY)
-        || ((y - 1 === nullY || y + 1 === nullY) && x === nullX)
-    ) {
-        arr[nullY][nullX] = arr[y][x];
-        arr[y][x] = 0;
-
-        emptyCell.left = x;
-        emptyCell.top = y;
+        document.querySelector('.field').appendChild(this.app.view);
     }
-}
 
-function getNullCell(){
-    for (let i = 0; i<4; i++){
-        for (let j=0; j<4; j++){
-            if(arr[j][i] === 0){
-                return {'x': i, 'y': j};
+    mix(stepCount) {
+        let x,y;
+
+        for (let i = 0; i < stepCount; i++) { // 5 итераций
+            let nullX = this._getNullCell().left;
+            let nullY = this._getNullCell().top;
+            /*console.log(`nullX: ${nullX}`);
+            console.log(`nullY: ${nullY}`);*/
+            let hMove = this._getRandomBool();
+            let upLeft = this._getRandomBool();
+            /*console.log(`hMove: ${hMove}`);
+            console.log(`upLeft: ${upLeft}`);*/
+            if (!hMove && !upLeft) { y = nullY; x = nullX - 1;}
+            if (hMove && !upLeft)  { x = nullX; y = nullY + 1;}
+            if (!hMove && upLeft)  { y = nullY; x = nullX + 1;}
+            if (hMove && upLeft)   { x = nullX; y = nullY - 1;}
+            /*console.log(`x: ${x}`);
+            console.log(`y: ${y}`);*/
+            if (0 <= x && x <= 3 && 0 <= y && y <= 3) {
+                this._shuffle(x, y);
             }
         }
     }
-}
 
-function mix(stepCount) {
-    let x,y;
+    placeBlocks() {
+        let numbers = this.arr.flat();
 
-    for (let i = 0; i < stepCount; i++) {
-        let nullX = this.getNullCell().x;
-        let nullY = this.getNullCell().y;
+        numbers.map((num, index) => {
+            const left = index % 4;
+            const top = (index - left) / 4;
 
-        let hMove = getRandomBool();
-        let upLeft = getRandomBool();
+            this._createCells(num, left, top);
+        });
+    }
 
-        console.log(hMove);
-        console.log(upLeft);
+    _put(cell, top, left, CELL_SIZE) { // _
+        cell.x = left * CELL_SIZE;
+        cell.y = top * CELL_SIZE;
 
-        if (!hMove && !upLeft) { y = nullY; x = nullX - 1;}
-        if (hMove && !upLeft)  { x = nullX; y = nullY + 1;}
-        if (!hMove && upLeft)  { y = nullY; x = nullX + 1;}
-        if (hMove && upLeft)   { x = nullX; y = nullY - 1;}
-        console.log(x, y);
-        if (0 <= x && x <= 3 && 0 <= y && y <= 3) {
-            shuffle(x, y);
+        cell.left = left;
+        cell.top = top;
+    }
+
+    _isWrongBlock(cell) { // _
+        const leftSubtraction = Math.abs(this.emptyCell.left - cell.left);
+        const topSubtractions = Math.abs(this.emptyCell.top - cell.top);
+
+        return leftSubtraction + topSubtractions > 1
+    }
+
+    _move(cell) { // используется для передвижения блоков рукой игрока
+        if (this._isWrongBlock(cell)) return;
+
+        Ease.ease.add(cell,{x: this.emptyCell.left * this.cellSize, y: this.emptyCell.top * this.cellSize}, { duration: 1000 } );
+
+        const tempLeft = this.emptyCell.left;
+        const tempTop = this.emptyCell.top;
+
+        this.emptyCell.left = cell.left;
+        this.emptyCell.top = cell.top;
+        cell.left = tempLeft;
+        cell.top = tempTop;
+
+        this._win();
+    }
+
+    _createCells(i, left, top) { //_
+        if (i === 0) return;
+
+        let cell = new PIXI.Sprite.from(`images/${i}.png`);
+        this._put(cell, top, left, this.cellSize);
+        this.app.stage.addChild(cell);
+
+        cell.interactive = true;
+        cell.buttonMode = true;
+        cell.index = i;
+
+        cell.on("pointerup", () => {
+            this._move(cell);
+        });
+
+        this.cells.push(cell);
+    }
+
+    _win() { // _
+        const isFinished = this.cells.every(cell => {
+            if (!cell.index) return true;
+            return cell.index === cell.top*4 + cell.left + 1; // cause we have a number
+        });
+
+        if (isFinished) alert("You win!")
+    }
+
+    _shuffle(x, y) { // используется для начального расставления блоков через двумерный массив arr
+        let nullX = this._getNullCell().left;
+        let nullY = this._getNullCell().top;
+        if (
+            ((x - 1 === nullX || x + 1 === nullX) && y === nullY)
+            || ((y - 1 === nullY || y + 1 === nullY) && x === nullX)
+        ) {
+            this.arr[nullY][nullX] = this.arr[y][x];
+            this.arr[y][x] = 0;
+
+            this.emptyCell.left = x;
+            this.emptyCell.top = y;
         }
     }
-}
 
-function getRandomBool() {
-    if (Math.floor(Math.random() * 2) === 0) {
-        return true;
+    _getNullCell(){ // refactor with emptyCell
+        return this.emptyCell;
     }
+
+    _getRandomBool() { // _
+        if (Math.floor(Math.random() * 2) === 0) {
+            return true; // функция возращает true, в обратном случае возвращает undefined, что является ложным значением
+        }
+    }
+
 }
 
-mix(30);
-
-let numbers = arr.flat();
-
-numbers.map((num, index) => {
-    const left = index % 4;
-    const top = (index - left) / 4;
-
-    createCells(num, left, top);
-});
+const puzzleGame15 = new PuzzleGame(200, 800, 800);
+puzzleGame15.createApp();
+puzzleGame15.mix(40);
+puzzleGame15.placeBlocks();
